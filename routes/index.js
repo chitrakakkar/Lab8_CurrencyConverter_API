86c8ab0c0f57ccf87210bdeb31effa9dc093e1f4
chitrakakkar <chitrakakkar@yahoo.co.in>
2017-02-27 15:29:12 -0600

    Conditions applied for same currency chosen

--- routes/index.js
+++ routes/index.js
@@ -14,18 +14,32 @@ router.get('/', function (req, res)
     res.render('index');
 
 });
+
+// router.get(function (error)
+// {
+//         if(error.responseText == 'showAlert')
+//             alert("Can't convert the same currency !! choose different currency forms")
+//
+// });
 /* Handle currency form submit */
 router.get('/convert', function(req, res)
 {
     var amounts = req.query.amount;
     var convert_from = req.query.from_currency;
     var convert_to = req.query.to_currency;
-    var rate_convertFrom = exchangeRates[convert_from];
-    var dollar_amount= amounts/rate_convertFrom ;
-    var rate_convertTo = exchangeRates[convert_to];
-    var results = dollar_amount*rate_convertTo;
-    console.log(results);
-    res.render('results', { Amount : amounts, result: results, from_currency:convert_from, to_currency: convert_to});
+    if(convert_to.toString() == convert_from.toString())
+    {
+        // res.send(error,'showAlert');
+        console.log("Can't convert the same currency !! choose different currency forms");
+    }
+    else {
+        var rate_convertFrom = exchangeRates[convert_from];
+        var dollar_amount = amounts / rate_convertFrom;
+        var rate_convertTo = exchangeRates[convert_to];
+        var results = dollar_amount * rate_convertTo;
+        console.log(results);
+        res.render('results', {Amount: amounts, result: results, from_currency: convert_from, to_currency: convert_to});
+    }
 
 });
 
