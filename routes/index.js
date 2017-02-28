360379a8128215eb4b9796a69e3054fdc7dc58a0
chitrakakkar <chitrakakkar@yahoo.co.in>
2017-02-28 09:33:35 -0600

    little documentation done

--- routes/index.js
+++ routes/index.js
@@ -28,6 +28,7 @@ router.get('/convert', function(req, res)
     var convert_from = req.query.from_currency;
     var convert_to = req.query.to_currency;
     var rate_convertFrom = exchangeRates[convert_from];
+    //converting everything to dollar
     var dollar_amount = amounts / rate_convertFrom;
     var rate_convertTo = exchangeRates[convert_to];
     var results = dollar_amount * rate_convertTo;
@@ -35,7 +36,7 @@ router.get('/convert', function(req, res)
     {
         // res.send(error,'showAlert');
         var message= ("Nothing changes !! choose different currency forms to see the difference !");
-        console.log("Can't convert the same currency !! choose different currency forms");
+        console.log("Nothing changes !! choose different currency forms to see the difference !");
         res.render('results', {Amount: amounts, result: results, from_currency: convert_from, to_currency: convert_to,message:message})
     }
 
